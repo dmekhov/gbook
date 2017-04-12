2511b43aeab165323c399e0ad80245ddca761e4a
dmekhov <dmekhov@gmail.com>
2017-04-12 15:55:48 +0300

    store message from user and guest + message list

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
 
     public function index()
     {
-        return view('comment.index');
+        $items = Comment::latest()->paginate(5);
+        return view('comment.index', compact('items'));
     }
 
     public function store(Request $request)
     {
-        dd($request->all());
+
+        $validator = Validator::make($request->all(), ['text'=>'required',]);
+
+        if ($validator->fails()) {
+            return redirect(url()->previous() . '#message-form')->withErrors($validator)->withInput();
+        }
+
+        if(Auth::user()) {
+            Auth::user()->comments()->create($request->all());
+        } else {
+            Comment::create($request->all());
+        }
+
+        return redirect('/');
     }
 }
--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -5,29 +5,25 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <h1>Guest Messages</h1>
-                <div class="panel panel-default">
-                    <div class="panel-heading">UserName</div>
 
-                    <div class="panel-body">
-                        Message
+                @foreach($items as $item)
+                    <div class="gbook-message panel panel-default">
+                        <div class="panel-heading">{{$item->user ? $item->user->name : 'Guest'}}</div>
+                        <div class="panel-body"><div class="gbook-message__text">{{$item->text}}</div></div>
                     </div>
-                </div>
+                @endforeach
 
-                <div class="panel panel-default">
-                    <div class="panel-heading">UserName</div>
+                {!! $items->render() !!}
 
-                    <div class="panel-body">
-                        Message
-                    </div>
-                </div>
+                @include('errors._list')
 
-                <div class="message-form panel panel-default">
+                <div class="panel panel-default" id="message-form">
                     <div class="panel-heading">{{Auth::guest() ? 'Guest' : Auth::user()->name}}</div>
                     <form class="panel-body" method="POST" action="{{ url('/comment') }}">
                         {{ csrf_field() }}
                         <div class="form-group">
                             <label for="text">Message</label>
-                            <textarea class="form-control" name="text" id="text" rows="3" required></textarea>
+                            <textarea class="form-control" name="text" id="text" rows="3" required>{{ old('text') }}</textarea>
                         </div>
                         <button type="submit" class="btn btn-default">Submit</button>
                     </form>
